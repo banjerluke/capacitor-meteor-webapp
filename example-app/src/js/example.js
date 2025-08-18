329ac9b83035bf75c9be3bb90d23bb89d5c1750e
Luke Abbott <luke@playingbyear.com>
2025-08-17 22:37:18 -0700

    We don't need no stinkin' example app...
    ...well, at least not for now. You can run `npx
    @capacitor/create-plugin` again to get the scaffold for an example app,
    though I think it would be tricky because we're probably better off
    using a real Meteor build for testing... no? Anyway, I'll be using a
    real Meteor app for testing for the time being.

--- example-app/src/js/example.js
+++
@@ -1,6 +0,0 @@
-import { CapacitorMeteorWebApp } from '@strummachine/capacitor-meteor-webapp';
-
-window.testEcho = () => {
-    const inputValue = document.getElementById("echoInput").value;
-    CapacitorMeteorWebApp.echo({ value: inputValue })
-}
