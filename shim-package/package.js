3c52892bbd40aa3d1b13f7b9295e334105718c27
Luke Abbott <luke@playingbyear.com>
2025-08-19 10:17:17 -0700

    Make shim package back-compat with Meteor 3.0

--- shim-package/package.js
+++ shim-package/package.js
@@ -1,13 +1,13 @@
 Package.describe({
   name: 'banjerluke:capacitor-meteor-webapp-shim',
-  version: '0.1.0',
+  version: '0.1.1',
   summary: 'Shims WebAppLocalServer to use the @banjerluke/capacitor-meteor-webapp plugin.',
   git: 'https://github.com/banjerluke/capacitor-meteor-webapp',
   documentation: 'README.md',
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom('3.3.1');
+  api.versionsFrom('3.0');
   api.use('ecmascript');
   api.addFiles('webapp_local_server_capacitor.js', 'web.cordova');
 });
